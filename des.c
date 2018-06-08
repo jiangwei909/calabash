a75f7dcf0c8fbffdf40ab8e4b8453425b9354d5a
jiangwei <jiangwei@pcidata.cn>
2018-06-08 15:46:59 +0800

    重构了DES的ECB加密

--- des.c
+++ des.c
@@ -65,43 +65,7 @@ static inline int des_ecb_crypt(const char* key, unsigned int key_len, const cha
 
 int des_ecb_encrypt(const char* key, unsigned int key_len, const char* plain, int plain_len, char* cipher)
 {
-    DES_key_schedule ks1,ks2,ks3;
-    
-    char key_block[8] = { 0x0 };
-    int cipher_len = 0;
-    
-    if (key_len % 8 != 0 || key_len > 24) return -1;
-    if (plain_len % 8 != 0) return -2;
-
-    memcpy(key_block, key, 8);
-    DES_set_key_unchecked((const_DES_cblock*)key_block, &ks1); 
-
-    if (key_len >= 16) {
-	memcpy(key_block, key+8, 8);
-	DES_set_key_unchecked((const_DES_cblock*)key_block, &ks2);
-    }
-
-    if (key_len >= 24) {
-	memcpy(key_block, key+16, 8);
-	DES_set_key_unchecked((const_DES_cblock*)key_block, &ks3);
-    }
-
-    for (int i = 0; i < plain_len / 8; i++) {
-	switch (key_len) {
-	case 8:
-	    DES_ecb_encrypt((const_DES_cblock*)(plain + i*8), (DES_cblock*)(cipher + i*8), &ks1, 1);
-	    break;
-	case 16:
-	    DES_ecb2_encrypt((const_DES_cblock*)(plain + i*8), (DES_cblock*)(cipher + i*8), &ks1, &ks2, 1);
-	      break;
-	case 24:
-	    DES_ecb3_encrypt((const_DES_cblock*)(plain + i*8), (DES_cblock*)(cipher + i*8), &ks1, &ks2, &ks3, 1);
-	      break;
-	}
-	cipher_len += 8;
-    }
-    
-    return cipher_len;
+    return des_ecb_crypt(key, key_len, plain, plain_len, cipher, 1);
 }
 
 
