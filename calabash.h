170642b237a41866ba2e68cc5d28a8d6eb7dfa82
jiangwei <jiangwei@pcidata.cn>
2018-06-26 10:37:53 +0800

    增加函数文档说明

--- calabash.h
+++ calabash.h
@@ -201,15 +201,35 @@ int sm4_cbc_encrypt(const char* key, const char* iv, const char* plain, int plai
  *  @param key 密钥的明文,必须16字节长
  *  @param iv 初始化向量,必须16字节长
  *  @param cipher 待解密的数据明文
- *  @param cipher_len 待加密的数据明文长度
- *  @param plain 加密后的密文
- *  @return 返回解密后解文的长度，负数表示失败
+ *  @param cipher_len 待解密的数据密文长度
+ *  @param plain 解密后的明文
+ *  @return 返回解密后明文的长度，负数表示失败
  */
 int sm4_cbc_decrypt(const char* key, const char* iv, const char* cipher, int cipher_len, char* plain);
 
 
+/**
+ *  @brief DES进行ECB加密
+ *  @details DES进行ECB加密, 密钥长度支持8/16/24字节，待加密的数据长度必须是8的倍数
+ *
+ *  @param key 密钥的明文,长度支持8/16/24字节
+ *  @param plain 待加密的数据明文
+ *  @param plain_len 待加密的数据明文长度
+ *  @param cipher 加密后的密文
+ *  @return 返回加密后的密文长度，负数表示失败
+ */
 int des_ecb_encrypt(const char* key, unsigned int key_len, const char* plain, int plain_len, char* cipher);
 
+/**
+ *  @brief DES进行ECB解密
+ *  @details DES进行ECB解密, 密钥长度支持8/16/24字节，待解密的数据长度必须是8的倍数
+ *
+ *  @param key 密钥的明文,长度支持8/16/24字节
+ *  @param cipher 待解密的数据密文
+ *  @param cipher_len 待解密的数据密文长度
+ *  @param plain 解密后的明文
+ *  @return 返回解密后的明文长度，负数表示失败
+ */
 int des_ecb_decrypt(const char* key, unsigned int key_len, const char* cipher, int plain_len, char* plain);
 
 
