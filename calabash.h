e906c23b311ec3dbbda26eb50d96d6f9cce11bd1
jiangwei <jiangwei@pcidata.cn>
2018-06-26 11:38:22 +0800

    增加函数文档说明

--- calabash.h
+++ calabash.h
@@ -262,7 +262,7 @@ int rsa_read_key_from_pem_str(const char* pem_str, int pem_str_len, char* puk);
  * @param pvk_len 生成的RSA私钥长度
  * @param puk 生成的RSA公钥
  * @param puk_len 生成的RSA公钥长度
- *@return 成功返回0，否则表示失败
+ * @return 成功返回0，否则表示失败
  */
 int rsa_generate_key(int bits, char* pvk, int* pvk_len, char* puk, int * puk_len);
 
@@ -273,7 +273,7 @@ int rsa_generate_key(int bits, char* pvk, int* pvk_len, char* puk, int * puk_len
  * @param puk 待转换的RSA公钥
  * @param puk_len 待转换的RSA公钥长度
  * @param pem_str 转换后的RSA公钥
- *@return 成功返回转换后的RSA公钥的长度，负数表示失败
+ * @return 成功返回转换后的RSA公钥的长度，负数表示失败
  */
 int rsa_dump_puk_to_pem_str(const char* puk, int puk_len, char* pem_str);
 
@@ -284,7 +284,7 @@ int rsa_dump_puk_to_pem_str(const char* puk, int puk_len, char* pem_str);
  * @param puk 待转换的RSA私钥
  * @param puk_len 待转换的RSA私钥长度
  * @param pem_str 转换后的RSA私钥
- *@return 成功返回转换后的RSA私钥的长度，负数表示失败
+ * @return 成功返回转换后的RSA私钥的长度，负数表示失败
  */
 int rsa_dump_pvk_to_pem_str(const char* pvk, int pvk_len, char* pem_str);
 
@@ -295,7 +295,7 @@ int rsa_dump_pvk_to_pem_str(const char* pvk, int pvk_len, char* pem_str);
  * @param puk 待转换的RSA公钥
  * @param puk_len 待转换的RSA公钥长度
  * @param file_name PEM文件
- *@return 成功返回PEM文件的长度，负数表示失败
+ * @return 成功返回PEM文件的长度，负数表示失败
  */
 int rsa_dump_puk_to_pem_file(const char* puk, int puk_len, char* file_name);
 
@@ -306,14 +306,60 @@ int rsa_dump_puk_to_pem_file(const char* puk, int puk_len, char* file_name);
  * @param pvk 待转换的RSA私钥
  * @param pvk_len 待转换的RSA私钥长度
  * @param file_name PEM文件
- *@return 成功返回PEM文件的长度，负数表示失败
+ * @return 成功返回PEM文件的长度，负数表示失败
  */
 int rsa_dump_pvk_to_pem_file(const char* pvk, int pvk_len, char* file_name);
 
+/**
+ * @brief 使用RSA公钥加密数据
+ * @details 使用RSA公钥加密数据
+ * 
+ * @param puk RSA公钥
+ * @param puk_len RSA公钥长度
+ * @param plain 待加密的明文
+ * @param plain_len 待加密的明文长度
+ * @param cipher 加密后的密文
+ * @return 成功返回加密后密文的长度，负数表示失败
+ */
 int rsa_encrypt(const char* puk, int puk_len, const char* plain, int plain_len, char* cipher);
 
+/**
+ * @brief 使用RSA私钥解密数据
+ * @details 使用RSA私钥解密数据
+ * 
+ * @param pvk RSA私钥
+ * @param pvk_len RSA私钥长度
+ * @param cipher 待解密的明文
+ * @param cipher_len 待解密的密文长度
+ * @param plain 解密后的明文
+ * @return 成功返回解密后明文的长度，负数表示失败
+ */
 int rsa_decrypt(const char* pvk, int pvk_len, const char* cipher, int cipher_len, char* plain);
 
+/**
+ * @brief 使用RSA私钥对数据进行签名
+ * @details 使用RSA私钥对数据进行签名
+ * 
+ * @param pvk RSA私钥
+ * @param pvk_len RSA私钥长度
+ * @param msg 待签名的数据
+ * @param msg_len 待签名的数据长度
+ * @param sign 签名值
+ * @return 成功返回签名值的长度，负数表示失败
+ */
 int rsa_sign(const char* pvk, int pvk_len, const char* msg, int msg_len, char* sign);
+
+/**
+ * @brief 使用RSA公钥对签名进行验证
+ * @details 使用RSA公钥对签名进行验证
+ * 
+ * @param puk RSA公钥
+ * @param puk_len RSA公钥长度
+ * @param msg 待验证的数据
+ * @param msg_len 待验证的数据长度
+ * @param sign 待验证的签名
+ * @param sign_len 待验证的签名长度
+ * @return 成功返回0，否则表示失败
+ */
 int rsa_verify(const char* puk, int puk_len, const char* msg, int msg_len, const char* sign, int sign_len);
 #endif //CALABASH_H
