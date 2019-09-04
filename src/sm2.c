8a6f5e235fc72c1c969f462c5a1f1041d7258980
Jiang Wei <jiangwei@example.com>
2019-09-04 11:23:15 +0800

    opt: opt in sm2 keypaire

--- src/sm2.c
+++ src/sm2.c
@@ -412,34 +412,37 @@ int sm2_read_pvk_from_pemfile(const char* pemfile, char* pvk, int* pvk_len)
 int cb_sm2_keypair(char* pk, char* sk)
 {
     EC_KEY* ec_key = NULL;
-    int ret = -1;
+    int ret = 0;
     BIGNUM* bn_pvk = NULL;
     EC_POINT* puk_point = NULL;
     EC_GROUP *curve_group = NULL;
     
     ec_key = EC_KEY_new_by_curve_name(NID_sm2p256v1);    
-    ret = EC_KEY_generate_key(ec_key);
+    int ec_ret = EC_KEY_generate_key(ec_key);
 
-    if (ret == 0) return -1;
+    if (ec_ret == 0) {
+        ret = -1;
+        goto end;
+    }
 
     bn_pvk = EC_KEY_get0_private_key(ec_key);
     BN_bn2bin(bn_pvk, sk);
-    BN_free(bn_pvk);
+    
 
     puk_point = EC_KEY_get0_public_key(ec_key);
     curve_group = EC_GROUP_new_by_curve_name(NID_sm2p256v1);
 
-    ret = EC_POINT_point2oct(curve_group, puk_point,
+    ec_ret = EC_POINT_point2oct(curve_group, puk_point,
 			     POINT_CONVERSION_UNCOMPRESSED,
-			     pk, 65, NULL);
+			     pk, CB_SM2_PUBLICKEY_BYTES, NULL);
+    
+    if (ec_ret != CB_SM2_PUBLICKEY_BYTES) ret = -2;
 
-    EC_POINT_free(puk_point);
+end:
     EC_GROUP_free(curve_group);
-    // EC_KEY_free(ec_key);
-
-    if (ret < 0) return ret;
-
-    return 0;
+    EC_KEY_free(ec_key);
+    
+    return ret;
 }
 
 int cb_sm2_encrypt(const char* pk, const char* plain, int plain_len, char* cipher)
