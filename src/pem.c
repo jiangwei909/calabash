bf2e6f343449ec6f8057e5c674c88d3956447121
Jiang Wei <jiangwei@example.com>
2019-09-04 17:20:56 +0800

    refactor:move sm2_read_pvk_from_pemfile to pem.c

--- src/pem.c
+++ src/pem.c
@@ -122,3 +122,52 @@ int sm2_read_puk_from_pem_str(const char* pem_str, int pem_str_len, char* puk, i
     BIO_free(fp);
     return 0;
 }
+
+
+int sm2_read_pvk_from_pemfile(const char* pemfile, char* pvk, int* pvk_len)
+{
+    BIO *fp;
+    char *name = 0;
+    char *header = 0;
+    unsigned char *buff = 0x0;
+    long buff_len;
+    int ret = -1;
+
+#ifdef WIN32
+    ret = _access(pemfile, 0);
+#else
+    ret = access(pemfile, R_OK);
+#endif
+    
+    if (ret != 0){
+        printf("Not found file: %s\n", pemfile);
+        return -1;
+    }
+
+    fp = BIO_new_file(pemfile, "r");
+    if (fp == NULL){
+        printf("Failed to open file: %s", pemfile);
+        ret = -2;
+        goto err;
+    }
+
+    ret = PEM_read_bio(fp, &name, &header, &buff, &buff_len);
+    if (strncmp(name, "EC PARAMETERS", 13) == 0) {
+	PEM_read_bio(fp, &name, &header, &buff, &buff_len);
+
+	 EVP_PKEY* pkey = d2i_AutoPrivateKey(NULL, &buff, buff_len);
+	 EC_KEY* ec_key = EVP_PKEY_get0_EC_KEY(pkey);
+	 const BIGNUM *prv_bn_cont = EC_KEY_get0_private_key(ec_key);
+
+	 *pvk_len = BN_bn2bin(prv_bn_cont, pvk);
+	 ret = 0;
+	 
+    } else {
+	printf("WARNING: This is not a EC key file.\n");
+	ret = -3;
+	goto err;
+    }
+  err:
+    BIO_free(fp);
+    return ret;
+}
