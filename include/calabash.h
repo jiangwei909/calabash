0d4b23b6b8788170e73b0910c5c63bb76f44ce05
Jiang Wei <jiangwei@example.com>
2019-09-20 16:37:39 +0800

    refactor: remove unneeded macro definition in calabash.h

--- include/calabash.h
+++ include/calabash.h
@@ -5,14 +5,6 @@
 #ifndef CALABASH_H
 #define CALABASH_H
 
-#define DIGEST_NONE 	0x0
-#define DIGEST_MD5  	0x4
-#define DIGEST_SHA1 	0x64
-#define DIGEST_SHA224 	0x675
-#define DIGEST_SHA256 	0x672
-#define DIGEST_SHA384 	0x673
-#define DIGEST_SHA512 	0x674
-
 #include "calabash/encode.h"
 #include "calabash/utils.h"
 #include "calabash/des.h"
